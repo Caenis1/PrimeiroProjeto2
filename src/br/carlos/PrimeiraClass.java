9e6cb7f207f004b31c69ca6f0e084879513f933a
Caenis1 <carlossocial1479@gmail.com>
2024-10-11 19:28:27 -0300

    Initial commit

---
+++ src/br/carlos/PrimeiraClass.java
@@ -0,0 +1,7 @@
+package br.carlos;
+
+public class PrimeiraClass {
+    public static void main (String[] args) {
+        System.out.println("Hello Carlos");
+    }
+}
---
+++ src/br/carlos/PrimeiraClasse.java
@@ -0,0 +1,6 @@
+package br.carlos;
+
+public class PrimeiraClasse {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+}
